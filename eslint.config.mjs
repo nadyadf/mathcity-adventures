a3a46522eb244d01f1236cfc9776129c9dfdd899
nadyadf <nadyadf3@gmail.com>
2024-07-06 00:14:49 +0700

    add config for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,18 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    languageOptions: { 
+      globals: {...globals.browser, ...globals.node} 
+    },
+    "rules": {
+      "import/no-extraneous-dependencies": 0,
+      "no-console": 0,
+      "no-underscore-dangle": 0,
+      "no-restricted-globals": 0,
+      "linebreak-style": 0
+    }
+  },
+    
   pluginJs.configs.recommended,
 ];
