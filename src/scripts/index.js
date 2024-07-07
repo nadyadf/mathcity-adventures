63fd86fd63bffbc57ec7486e736b87924878824b
nadyadf <nadyadf3@gmail.com>
2024-07-07 22:11:30 +0700

    create app shell rendering

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,18 @@ import 'regenerator-runtime';
 import '../styles/variables.css';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('#humberger-button'),
+  drawer: document.querySelector('#navigation-drawer'),
+  content: document.querySelector('#content'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+})
