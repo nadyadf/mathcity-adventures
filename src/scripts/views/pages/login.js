13e9d94112ccb24b0c52718938a4400d1e71be8c
nadyadf <nadyadf3@gmail.com>
2024-07-14 19:07:25 +0700

    create login page

--- src/scripts/views/pages/login.js
+++ src/scripts/views/pages/login.js
@@ -1,12 +1,41 @@
+import AuthService from "../../utils/auth-service";
+
 const Login = {
   async render() {
     return `
-      <h1>Ini adalah halaman login</h1>
+    <div class="login-container">
+      <img src="app-title.png">
+      <form class="login-form">
+        <h2>LOGIN</h2>
+        <div class="input-user">
+          <input id="username" placeholder="Username" autocomplete="off"/>
+          <input id="password" type="password" placeholder="Password"/>
+        </div>
+        <button role="button" id="btn-login">LOGIN</button>
+      </form>
+      <div class="signup-offer">
+        <p>Belum memiliki akun?</p>
+        <button>SIGN UP</button>
+      </div>
+    </div>
     `;
   },
 
   async afterRender() {
+    const btnLogin = document.getElementById('btn-login');
+    btnLogin.addEventListener('click', (event) => {
+      event.preventDefault();
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
 
+      const authUser = new AuthService;
+      authUser.login(username, password);
+
+      if (authUser.isLoggedIn) {
+        window.location.href = "/";
+      }
+
+    })
   },
 };
 
