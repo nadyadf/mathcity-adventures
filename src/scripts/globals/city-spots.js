4daa4258c5bdfd1f9e5dc5c0a35958ed895053fb
nadyadf <nadyadf3@gmail.com>
2024-08-11 15:14:33 +0700

    create cityspots array

---
+++ src/scripts/globals/city-spots.js
@@ -0,0 +1,29 @@
+const CitySpots = [
+  {
+    cityName: 'Green Land',
+    spots: [
+      {
+        imgUrl: 'taman-lumina.jpg',
+        spotName: 'Taman Lumina',
+      },
+      {
+        imgUrl: 'rumah-pohon-horizon.jpg',
+        spotName: 'Rumah Pohon Horizon',
+      },
+      {
+        imgUrl: 'kanal-serenity.jpg',
+        spotName: 'Kanal Serenity',
+      },
+      {
+        imgUrl: 'jembatan-ever-green.jpg',
+        spotName: 'Jembatan Ever Green',
+      },
+      {
+        imgUrl: 'jalan-kaspia.jpg',
+        spotName: 'Jalan Kaspia',
+      },
+    ],
+  },
+];
+
+export default CitySpots;
